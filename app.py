6a50eb49fb62bcdc44b8d25ddcb2a363d0eabb1f
oct <re.oct.murtu@gmail.com>
2026-02-07 19:07:54 +0900

    first commit

--- app.py
+++ app.py
@@ -3,7 +3,7 @@ from dotenv import load_dotenv
 import streamlit as st
 
 from langchain_openai import ChatOpenAI
-from langchain.schema import SystemMessage, HumanMessage, AIMessage
+from langchain_core.messages import SystemMessage, HumanMessage, AIMessage
 
 load_dotenv()
 
